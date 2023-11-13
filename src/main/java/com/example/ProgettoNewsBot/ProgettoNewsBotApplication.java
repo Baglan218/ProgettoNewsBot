4c1bb1d94087b7ba925e32f371d06d64250a0a71
Baglan218 <abdikhani040502@gmail.com>
2023-11-13 23:16:56 +0600

    Initial commit

---
+++ src/main/java/com/example/ProgettoNewsBot/ProgettoNewsBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoNewsBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoNewsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoNewsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgettoNewsBot/ProgettoNewsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoNewsBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoNewsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
